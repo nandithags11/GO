b9f6817ec3c9894dd18c6f9d77fb8470b4c89179
Nanditha G S <nandithags11@gmail.com>
2020-12-23 22:01:51 +0530

    hash A 2 zzz

--- trial.go
+++ trial.go
@@ -1,22 +1,51 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 )
 
+var hash string
+
 func main() {
 	for i := 'A'; i <= 'Z'; i++ {
-		s_i := string(i)
-		fmt.Println(s_i)
+		to_hash := string(i)
+		hash = hasher(to_hash)
+		fmt.Println(hash)
 		if i == 'Z' {
 			for i := 'A'; i <= 'Z'; i++ {
 				for j := 'A'; j <= 'Z'; j++ {
 					s2_i := string(i)
 					s2_j := string(j)
-					ii := s2_i + s2_j
-					fmt.Println(ii)
+					to_hash := s2_i + s2_j
+					t_hash := to_hash
+					hash = hasher(to_hash)
+					fmt.Println(hash)
+
+					if t_hash == "ZZ" {
+						for i := 'A'; i <= 'Z'; i++ {
+							for j := 'A'; j <= 'Z'; j++ {
+								for k := 'A'; k <= 'Z'; k++ {
+									s3_i := string(i)
+									s3_j := string(j)
+									s3_k := string(k)
+									to_hash := s3_i + s3_j + s3_k
+									hash = hasher(to_hash)
+									fmt.Printf("%s    %s \n", to_hash, hash)
+								}
+							}
+						}
+					}
 				}
 			}
 		}
 	}
+}
+func hasher(to_hash string) string {
+	h := md5.New()
+	h.Write([]byte(string(to_hash)))
+	hash := hex.EncodeToString(h.Sum(nil))
+	return hash
+
 }
